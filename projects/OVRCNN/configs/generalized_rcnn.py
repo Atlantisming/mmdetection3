24d320b4a53df320f1c8d3f9382d373969bff5be
Atlantisming <505642573@qq.com>
2023-10-09 11:32:40 +0800

    update config.

--- projects/OVRCNN/configs/generalized_rcnn.py
+++ projects/OVRCNN/configs/generalized_rcnn.py
@@ -3,6 +3,7 @@ _base_ = [
     'mmdet::_base_/default_runtime.py'
 ]
 
+num_classes = 48
 norm_cfg = dict(type='BN', requires_grad=False)
 
 custom_imports = dict(
@@ -18,7 +19,8 @@ model = dict(
         # mean=[102.9801, 115.9465, 122.7717],
         std=[1.0, 1.0, 1.0],
         bgr_to_rgb=False,
-        pad_size_divisor=32),
+        # pad_size_divisor=32,
+    ),
     backbone=dict(
         type='ResNet',
         depth=50,
@@ -30,9 +32,10 @@ model = dict(
         norm_cfg=norm_cfg,
         norm_eval=True,
         style='caffe',
-        init_cfg=dict(
-            type='Pretrained',
-            checkpoint='open-mmlab://detectron2/resnet50_caffe')),
+        # init_cfg=dict(
+        #     type='Pretrained',
+        # checkpoint='open-mmlab://detectron2/resnet50_caffe')
+    ),
     rpn_head=dict(
         type='RPNHead',
         in_channels=1024,
@@ -41,14 +44,16 @@ model = dict(
             type='AnchorGenerator',
             scales=[2, 4, 8, 16, 32],
             ratios=[0.5, 1.0, 2.0],
-            strides=[16]),
+            strides=[16],
+            centers=[(7.5, 7.5)],
+        ),
         bbox_coder=dict(
             type='DeltaXYWHBBoxCoder',
             target_means=[.0, .0, .0, .0],
             target_stds=[1.0, 1.0, 1.0, 1.0]),
         loss_cls=dict(
             type='CrossEntropyLoss', use_sigmoid=True, loss_weight=1.0),
-        loss_bbox=dict(type='L1Loss', loss_weight=1.0)),
+        loss_bbox=dict(type='SmoothL1Loss', loss_weight=1.0, beta=1.0 / 9)),
     roi_head=dict(
         type='StandardRoIHead',
         shared_head=dict(
@@ -60,28 +65,44 @@ model = dict(
             style='caffe',
             norm_cfg=norm_cfg,
             norm_eval=True,
-            init_cfg=dict(
-                type='Pretrained',
-                checkpoint='open-mmlab://detectron2/resnet50_caffe')),
+            # init_cfg=dict(
+            #     type='Pretrained',
+            #     checkpoint='open-mmlab://detectron2/resnet50_caffe',
+            # )
+        ),
         bbox_roi_extractor=dict(
             type='SingleRoIExtractor',
-            roi_layer=dict(type='RoIAlign', output_size=14, sampling_ratio=0),
+            roi_layer=dict(
+                type='RoIAlign',
+                output_size=14,
+                sampling_ratio=0,
+                pool_mode='avg',
+                aligned=False,
+                # aligned=True,
+            ),
             out_channels=1024,
             featmap_strides=[16]),
         bbox_head=dict(
             type='V2LBBoxHead',
             with_avg_pool=True,
+            cls_weight_path='./datasets/zeroshot_coco/'
+            'zero-shot/seen_class_weight.npy',
+            # cls_weight_path='./datasets/zeroshot_coco/zero-shot/unseen_class_weight.npy',
             roi_feat_size=7,
             in_channels=2048,
-            num_classes=48,
+            num_classes=num_classes,
+            with_cls=False,
             bbox_coder=dict(
                 type='DeltaXYWHBBoxCoder',
                 target_means=[0., 0., 0., 0.],
                 target_stds=[0.1, 0.1, 0.2, 0.2]),
             reg_class_agnostic=True,
             loss_cls=dict(
-                type='CrossEntropyLoss', use_sigmoid=False, loss_weight=1.0),
-            loss_bbox=dict(type='L1Loss', loss_weight=1.0))),
+                type='CrossEntropyLoss',
+                use_sigmoid=False,
+                loss_weight=1.0,
+                class_weight=[1.] * (num_classes) + [0.2]),
+            loss_bbox=dict(type='SmoothL1Loss', loss_weight=1.0, beta=1.0))),
     # model training and testing settings
     train_cfg=dict(
         rpn=dict(
@@ -89,7 +110,7 @@ model = dict(
                 type='MaxIoUAssigner',
                 pos_iou_thr=0.7,
                 neg_iou_thr=0.3,
-                min_pos_iou=0.3,
+                min_pos_iou=0.,
                 match_low_quality=True,
                 ignore_iof_thr=-1),
             sampler=dict(
@@ -112,14 +133,14 @@ model = dict(
                 pos_iou_thr=0.5,
                 neg_iou_thr=0.5,
                 min_pos_iou=0.5,
-                match_low_quality=True,
+                match_low_quality=False,
                 ignore_iof_thr=-1),
             sampler=dict(
                 type='RandomSampler',
                 num=512,
-                pos_fraction=0.25,
+                pos_fraction=1,
                 neg_pos_ub=-1,
-                add_gt_as_proposals=True),
+                add_gt_as_proposals=False),
             pos_weight=-1,
             debug=False)),
     test_cfg=dict(
@@ -132,20 +153,78 @@ model = dict(
             score_thr=0.05,
             nms=dict(type='nms', iou_threshold=0.5),
             max_per_img=100,
-            mask_thr_binary=0.5)))
+        )))
 
 img_norm_cfg = dict(
-    mean=[103.530, 116.280, 123.675], std=[1.0, 1.0, 1.0], to_rgb=False)
+    mean=[103.530, 116.280, 123.675],
+    # detectron mean
+    # mean=[102.9801, 115.9465, 122.7717],
+    std=[1.0, 1.0, 1.0],
+    to_rgb=False)
 train_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(type='LoadAnnotations', with_bbox=True),
     dict(type='Resize', img_scale=(1333, 800), keep_ratio=True),
-    dict(type='RandomFlip', flip_ratio=0.5),
+    dict(
+        type='RandomFlip',
+        prob=0.75,
+        direction=['horizontal', 'vertical', 'diagonal']),
     dict(type='Normalize', **img_norm_cfg),
     dict(type='Pad', size_divisor=32),
     dict(type='DefaultFormatBundle'),
     dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels']),
 ]
-data = dict(train=dict(pipeline=train_pipeline), )
+# test_pipeline = [
+#     dict(type='LoadImageFromFile'),
+#     dict(
+#         type='MultiScaleFlipAug',
+#         img_scale=(1333, 800),
+#         flip=False,
+#         transforms=[
+#             dict(type='Resize', keep_ratio=True),
+#             dict(type='RandomFlip'),
+#             dict(type='Normalize', **img_norm_cfg),
+#             dict(type='Pad', size_divisor=32),
+#             dict(type='ImageToTensor', keys=['img']),
+#             dict(type='Collect', keys=['img']),
+#         ])
+# ]
+data = dict(
+    train=dict(pipeline=train_pipeline),
+    # val=dict(pipeline=test_pipeline),
+    # test=dict(pipeline=test_pipeline)
+)
 # optimizer
-optimizer = dict(type='SGD', lr=0.02, momentum=0.9, weight_decay=0.0001)
+optim_wrapper = dict(
+    type='OptimWrapper',
+    optimizer=dict(
+        _delete_=True,
+        type='SGD',
+        lr=0.005,
+        momentum=0.9,
+        weight_decay=0.0001,
+    ),
+    paramwise_cfg=dict(bias_lr_mult=2, ),
+)
+
+train_cfg = dict(
+    _delete_=True,
+    type='IterBasedTrainLoop',
+    max_iters=150000,
+    val_interval=10000)
+
+param_scheduler = [
+    dict(
+        type='LinearLR', start_factor=0.01, by_epoch=False, begin=0, end=1000),
+    dict(
+        type='MultiStepLR',
+        begin=0,
+        end=150000,
+        by_epoch=False,
+        milestones=[60000, 120000],
+        gamma=0.1)
+]
+
+default_hooks = dict(
+    checkpoint=dict(by_epoch=False, interval=10000, max_keep_ckpts=3))
+log_processor = dict(by_epoch=False)
--- projects/OVRCNN/configs/zeroshot_coco.py
+++ projects/OVRCNN/configs/zeroshot_coco.py
@@ -5,13 +5,26 @@ _base_ = [
 dataset_type = 'CocoDataset'
 data_root = './datasets/zeroshot_coco/'
 
+seen_class = ('person', 'bicycle', 'car', 'motorcycle', 'train', 'truck',
+              'boat', 'bench', 'bird', 'horse', 'sheep', 'bear', 'zebra',
+              'giraffe', 'backpack', 'handbag', 'suitcase', 'frisbee', 'skis',
+              'kite', 'surfboard', 'bottle', 'fork', 'spoon', 'bowl', 'banana',
+              'apple', 'sandwich', 'orange', 'broccoli', 'carrot', 'pizza',
+              'donut', 'chair', 'bed', 'toilet', 'tv', 'laptop', 'mouse',
+              'remote', 'microwave', 'oven', 'toaster', 'refrigerator', 'book',
+              'clock', 'vase', 'toothbrush')
+
+unseen_class = ('umbrella', 'cow', 'cup', 'bus', 'keyboard', 'skateboard',
+                'dog', 'couch', 'tie', 'snowboard', 'sink', 'elephant', 'cake',
+                'scissors', 'airplane', 'cat', 'knife')
+
 file_client_args = dict(backend='disk')
 train_pipeline = [
     dict(type='LoadImageFromFile', file_client_args=file_client_args),
     dict(type='LoadAnnotations', with_bbox=True),
     dict(type='Resize', scale=(1333, 800), keep_ratio=True),
     dict(type='RandomFlip', prob=0.5),
-    dict(type='PackDetInputs')
+    dict(type='PackDetInputs', )
 ]
 test_pipeline = [
     dict(type='LoadImageFromFile', file_client_args=file_client_args),
@@ -39,6 +52,7 @@ train_dataloader = dict(
         type=dataset_type,
         data_root=data_root,
         ann_file='zero-shot/instances_train2017_seen_2.json',
+        metainfo=dict(classes=seen_class),
         data_prefix=dict(img='train2017/'),
         filter_cfg=dict(filter_empty_gt=True, min_size=32),
         pipeline=train_pipeline))
@@ -52,6 +66,8 @@ val_dataloader = dict(
         type=dataset_type,
         data_root=data_root,
         ann_file='zero-shot/instances_val2017_seen_2.json',
+        metainfo=dict(classes=seen_class),
+        # ann_file='zero-shot/instances_val2017_unseen_2.json',
         data_prefix=dict(img='val2017/'),
         test_mode=True,
         pipeline=test_pipeline))
@@ -60,6 +76,7 @@ test_dataloader = val_dataloader
 val_evaluator = dict(
     type='CocoMetric',
     ann_file=data_root + 'zero-shot/instances_val2017_seen_2.json',
+    # ann_file=data_root + 'zero-shot/instances_val2017_unseen_2.json',
     metric='bbox',
     format_only=False)
 test_evaluator = val_evaluator
